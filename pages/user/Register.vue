e40822722c5ff4f2ce2719b42230f202e5694bd2
tarunama <sakeharami@gmail.com>
2019-12-09 16:43:34 +0900

    feat(User/register): Add component

---
+++ pages/user/Register.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Register</h1>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Register extends Vue {}
+</script>
